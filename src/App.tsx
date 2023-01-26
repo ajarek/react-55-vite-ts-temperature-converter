4278cca51bcad1bf6f775c529cdbc24b42199e9f
Ajarek <ajarek@poczta.onet.pl>
2023-01-26 21:25:23 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <div className="div">50</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">40</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">30</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">20</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">10</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">0</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">-10</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">-20</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">-30</div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="mala"></div>
+     <div className="div">-40</div>
+    </div>
+  )
+}
+
+export default App
